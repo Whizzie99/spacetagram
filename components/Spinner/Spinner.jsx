bb4759a0176c91e3671893e382a07aa01286b8d0
Whizzie99 <47413415+Whizzie99@users.noreply.github.com>
2022-01-11 14:43:18 +0100

    change spinner img src

--- components/Spinner/Spinner.jsx
+++ components/Spinner/Spinner.jsx
@@ -1,13 +1,13 @@
 import Image from "next/image";
 import { StyledWrapper } from "./styles";
 
-import test from '../../public/images/test.gif'
+// import test from '../../public/images/test.gif'
 
 const Spinner = () => {
     return (
         <StyledWrapper>
             <div>
-                <Image src={test} objectFit="cover" layout="fill" alt="testing" priority="true" />
+                <Image src="https://media0.giphy.com/media/hoIcq8JGrUeYHoUHuR/giphy.gif?cid=790b761161004beb73d84db3ec529f02c9809f62a05bce8e&rid=giphy.gif&ct=s" objectFit="cover" layout="fill" alt="loading space rocket" priority="true" />
             </div>
         </StyledWrapper>
     );
--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ['apod.nasa.gov', 'www.youtube.com', 'player.vimeo.com'],
+    domains: ['apod.nasa.gov', 'www.youtube.com', 'player.vimeo.com', 'giphy.com', 'media0.giphy.com'],
   }
 }
